5ce3c1206b6159e50219d7f5e5ed32b88ee7e275
xaviguasch <xaviguasch@gmail.com>
2022-06-05 20:47:54 +0200

    processed responses from POST requests

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response')
+})
+app.post('/tacos', (req, res) => {
+  const { meat, qty } = req.body
+  res.send(`Ok, here are your ${qty} ${meat}s.`)
 })
 
 app.listen(3000, () => {
