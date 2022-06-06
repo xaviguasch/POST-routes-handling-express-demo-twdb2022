43eebebae3ea224e4ab8f5a0d913b1e3eac1a4cc
xaviguasch <xaviguasch@gmail.com>
2022-06-06 10:37:23 +0200

    served comments via EJS and RESTful methodology

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
+//To parse form data in POST request body:
 app.use(express.urlencoded({ extended: true }))
+// To parse incoming JSON in POST request body:
 app.use(express.json())
+// Views folder and EJS setup:
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Our fake database:
+let comments = [
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog',
+  },
+  {
+    username: 'Sk8erBoi',
+    comment: 'Plz delete your account, Todd',
+  },
+  {
+    username: 'onlysayswoof',
+    comment: 'woof woof woof',
+  },
+]
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments })
+})
 
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response')
